5bf8e551904d3d91b8a639dc20303dada935fdb2
sco-bo <scottebobbitt@gmail.com>
2016-03-02 17:43:57 -0500

    create passing airport and flight model tests

--- test/models/airport_test.rb
+++ test/models/airport_test.rb
@@ -1,7 +1,22 @@
 require 'test_helper'
 
 class AirportTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  def setup
+    @flight = flights(:two)
+    @airport = airports(:iad)
+    @return_flight = flights(:four)
+  end
+
+  test "should be valid" do 
+    assert @airport.valid?
+  end
+
+  test "departing flights should include appropriate flight" do 
+    assert @airport.departing_flights.include?(@flight)
+  end
+
+  test "arriving flights should include appropriate flight" do 
+    assert @airport.arriving_flights.include?(@return_flight)
+  end
 end
--- test/models/flight_test.rb
+++ test/models/flight_test.rb
@@ -1,7 +1,22 @@
 require 'test_helper'
 
 class FlightTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+ 
+  def setup
+    @from_airport = airports(:lax)
+    @to_airport = airports(:ord)
+    @flight = flights(:one)
+  end
+
+  test "should be valid" do 
+    @flight.valid?
+  end
+
+  test "from airport is correct" do 
+    assert_equal @flight.from_airport_id, @from_airport.id
+  end
+
+  test "to airport is correct" do 
+    assert_equal @flight.to_airport_id, @to_airport.id
+  end
 end
