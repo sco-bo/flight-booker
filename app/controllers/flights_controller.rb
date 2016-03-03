40bcd1a9be988db9733e3bc8f0671e3eda79886c
sco-bo <scottebobbitt@gmail.com>
2016-03-03 11:08:09 -0500

    add flight search view and parameters

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,2 +1,11 @@
 class FlightsController < ApplicationController
+
+  def index
+    @airport_options = Airport.all.map{|i| [i.code, i.id] }
+    @date_options = Flight.all.sorted.map{|n| [n.date_formatted, n.id] }
+  end
+
+  def new
+
+  end
 end
--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,4 +1,12 @@
 class Flight < ActiveRecord::Base
   belongs_to :from_airport, class_name: "Airport"
   belongs_to :to_airport, class_name: "Airport"
+
+  def date_formatted
+    self.start_time.strftime("%a %b %d, %Y - %l:%M%P")
+  end
+
+  def self.sorted
+    self.order(:start_time)
+  end
 end
